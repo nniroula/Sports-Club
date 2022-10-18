b010255d64155f2925b1959600ba5494f3019535
nabin <nkbhaie2017@gmail.com>
2022-10-18 11:17:34 -0600

    update the tables

--- SQLs/dbSchemas.sql
+++ SQLs/dbSchemas.sql
@@ -6,13 +6,9 @@ CREATE TABLE players (
     id SERIAL PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-     -- username VARCHAR(25) PRIMARY KEY,
-    -- password TEXT NOT NULL,
-    email TEXT NOT NULL
-    CHECK (position('@' IN email) > 1),
-    -- is_admin BOOLEAN NOT NULL DEFAULT FALSE
+    email TEXT NOT NULL CHECK (position('@' IN email) > 1),
     birth_date TEXT NOT NULL,
-    phone_number TEXT NOT NULL,
+    phone_number TEXT UNIQUE NOT NULL,
     emergency_contact TEXT NOT NULL,
     profile_picture_url TEXT,
     playing_role TEXT,
