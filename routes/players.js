eb8cad5e868911c3bff5037706c12adfb1596d63
nabin <nkbhaie2017@gmail.com>
2022-10-18 20:13:58 -0600

    refactor the player.js file

--- routes/players.js
+++ routes/players.js
@@ -2,19 +2,17 @@
 
 const { json } = require('express');
 const express = require('express');
-// const app = require('../app');   // Instead router is needed
-
-// import db to query the database
-const db = require('../db');  // db = db.js file
-// import playerClass
+const jsonschema = require('jsonschema');
+const playerSchema = require('../schemas/playerSchema');
+const db = require('../db');  
 const Player = require('../models/playerClass');
+const { BadRequestError } = require("../errors/expressErrors");
 
 const router = new express.Router();
 
 // HOME Route
 router.get('/', function(req, res, next){
     console.log("This the home route for the app.");
-    // return res.send("Aurora Everest Cricket Club.");
     return res.send({
         "name": "Aurora Everest Cricket Club",
         "Estd": "July 2020"
@@ -31,9 +29,10 @@ router.get('/players', async function(req, res, next){
     }
 })
 
+
 // get a single player
 router.get('/players/:id', async function(req, res, next){
-    // invalid id should throw error, it does not do that now. handle it in getPlayerById method
+    // invalid id should throw error, it does not do that. handle it in getPlayerById method
     try{
         const result = await Player.getPlayerById(req.params.id);
         return res.json(result);
@@ -45,23 +44,32 @@ router.get('/players/:id', async function(req, res, next){
 
 // post request
 router.post('/players', async function(req, res, next){
-    // grab data passed in req.body, and that data to create a player
-    // destructure the request body and get data
+    // NEW Version with jsonschema
     try{
-        const {first_name, last_name, email, birth_date, 
-            phone_number, emergency_contact, profile_picture_url,
-            playing_role, registered_date} = req.body;
+        const validatedInput = jsonschema.validate(req.body, playerSchema);
 
-        const player = await Player.createPlayer(first_name, last_name, email, birth_date, 
-            phone_number, emergency_contact, profile_picture_url,
-            playing_role, registered_date);
+        if(!validatedInput.valid){
+            // return res.json("invalid input.")
+            const errs = validatedInput.errors.map(e => e.stack);
+            throw new BadRequestError(errs);                      //FIX this issue returns everything, not just error
+            // return new BadRequestError(errs); // runs forever
+        }
+
+        const {first_name, last_name, email, birth_date, phone_number, emergency_contact, profile_picture_url,
+                playing_role, registered_date} = req.body;
+
+        const player = await Player.createPlayer(first_name, last_name, email, birth_date, phone_number, 
+                                    emergency_contact, profile_picture_url, playing_role, registered_date);
+
+        // const player = Player.createPlayer(req.body);
         
-        // return res.json(player);
         return res.status(201).json(player);
+
     }catch(e){
         return next(e);
     }
 })
+
 
 // update the player
 router.put('/players/:id', async function(req, res, next){
@@ -84,12 +92,10 @@ router.put('/players/:id', async function(req, res, next){
     }
 })
 
+
 // delete a player
 router.delete('/players/:id', async function(req, res, next){
     try{
-        // const result = db.query('DELETE FROM players WHERE id = $1', [req.params.id]);
-        // return res.send({message: "Successfully deleted a user"});
-
         // destructure the request params, and get the id
         const id = req.params.id;
         const result = await Player.deletePlayer(id);
@@ -99,132 +105,6 @@ router.delete('/players/:id', async function(req, res, next){
     }
 })
 
-
 module.exports = router;
-
-
-//
-
-
-/** Routes for companies. */
-
-/*
-const jsonschema = require("jsonschema");
-const express = require("express");
-
-const { BadRequestError } = require("../expressError");
-const { ensureLoggedIn } = require("../middleware/auth");
-const Company = require("../models/company");
-
-const companyNewSchema = require("../schemas/companyNew.json");
-const companyUpdateSchema = require("../schemas/companyUpdate.json");
-
-// const router = new express.Router();
-*/
-
-/** POST / { company } =>  { company }
- *
- * company should be { handle, name, description, numEmployees, logoUrl }
- *
- * Returns { handle, name, description, numEmployees, logoUrl }
- *
- * Authorization required: login
- */
-/*
-router.post("/", ensureLoggedIn, async function (req, res, next) {
-  try {
-    const validator = jsonschema.validate(req.body, companyNewSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
-    }
-
-    const company = await Company.create(req.body);
-    return res.status(201).json({ company });
-  } catch (err) {
-    return next(err);
-  }
-});
-*/
-/** GET /  =>
- *   { companies: [ { handle, name, description, numEmployees, logoUrl }, ...] }
- *
- * Can filter on provided search filters:
- * - minEmployees
- * - maxEmployees
- * - nameLike (will find case-insensitive, partial matches)
- *
- * Authorization required: none
- */
-/*
-router.get("/", async function (req, res, next) {
-  try {
-    // const companies = await Company.findAll(); // OLD CODE
-    const companies = await Company.findAll(req.query); // new code
-    return res.json({ companies });
-  } catch (err) {
-    return next(err);
-  }
-});
-*/
-/** GET /[handle]  =>  { company }
- *
- *  Company is { handle, name, description, numEmployees, logoUrl, jobs }
- *   where jobs is [{ id, title, salary, equity }, ...]
- *
- * Authorization required: none
- */
-/*
-router.get("/:handle", async function (req, res, next) {
-  try {
-    const company = await Company.get(req.params.handle);
-    return res.json({ company });
-  } catch (err) {
-    return next(err);
-  }
-});
-*/
-/** PATCH /[handle] { fld1, fld2, ... } => { company }
- *
- * Patches company data.
- *
- * fields can be: { name, description, numEmployees, logo_url }
- *
- * Returns { handle, name, description, numEmployees, logo_url }
- *
- * Authorization required: login
- */
-/*
-router.patch("/:handle", ensureLoggedIn, async function (req, res, next) {
-  try {
-    const validator = jsonschema.validate(req.body, companyUpdateSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
-    }
-
-    const company = await Company.update(req.params.handle, req.body);
-    return res.json({ company });
-  } catch (err) {
-    return next(err);
-  }
-});
-*/
-/** DELETE /[handle]  =>  { deleted: handle }
- *
- * Authorization: login
- */
-/*
-router.delete("/:handle", ensureLoggedIn, async function (req, res, next) {
-  try {
-    await Company.remove(req.params.handle);
-    return res.json({ deleted: req.params.handle });
-  } catch (err) {
-    return next(err);
-  }
-});
-*/
-
-// module.exports = router;
 
 
