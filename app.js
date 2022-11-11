fc927a5b191ab06a85734ead44164f5d61a6a92b
nabin <nkbhaie2017@gmail.com>
2022-11-10 19:51:22 -0700

    refactor and clean the code

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
 "use strict";
 
 const express = require('express');
-// import routes
 const playersRoute = require('./routes/players');
 const usersRoute = require('./routes/users');
-
 const gamesRoute = require('./routes/games');
-// import error class
-const { NotFoundError, ExpressError } = require("./errors/expressErrors");
-
-// import the middleware routes
-const middlewareRoute = require('./middleware/auth');
 const authRoute = require('./routes/auth');
 
 const app = express();
 
-// use the players route
-// app.use('/', playersRoute);
-
-app.use(express.json()); // parse request bodies for json
+app.use(express.json());
 app.use('', playersRoute);
-// app.use(ExpressError)
 app.use('/users', usersRoute);
-// use middleware rotue
-// app.use('/user', middlewareRoute);
 app.use('/user', authRoute);
 app.use('/games', gamesRoute);
-
-
-
-
-/** Handle 404 errors -- this matches everything */
-// app.use(function (req, res, next) {
-//     return next(new NotFoundError());
-// });
 
 /** Generic error handler; anything unhandled goes here. */
 app.use(function (err, req, res, next) {
@@ -46,42 +25,6 @@ app.use(function (err, req, res, next) {
     });
 });
 
-// app.listen(3000, function(){
-//     console.log("Server started on the port 3000");
-// })
-
 
 module.exports = app;
 
-
-// 
-/*
-
-const express = require("express");
-const cors = require("cors");
-
-const { NotFoundError } = require("./expressError");
-
-const { authenticateJWT } = require("./middleware/auth");
-const authRoutes = require("./routes/auth");
-const companiesRoutes = require("./routes/companies");
-const usersRoutes = require("./routes/users");
-
-const morgan = require("morgan");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(morgan("tiny"));
-app.use(authenticateJWT);
-
-app.use("/auth", authRoutes);
-app.use("/companies", companiesRoutes); // this is only "/" in companies.js file
-app.use("/users", usersRoutes);
-
-
-
-
-module.exports = app;
-*/
--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 
 const app = require("./app");
 const { PORT } = require("./configs/configurations");
-// const PORT = require("./configs/configurations");
-
 
 app.listen(PORT, function () {
     console.log(`Server started on http://localhost:${PORT}`);
