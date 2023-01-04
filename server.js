9aef8aa6e286b8f36c88cb4f63e06bf8c3c31ade
nabin <nkbhaie2017@gmail.com>
2023-01-04 16:16:18 -0700

    clean up the server.js file

--- server.js
+++ server.js
@@ -3,14 +3,6 @@
 const app = require("./app");
 const { PORT } = require("./configs/configurations");
 
-// if(process.env.NODE_ENV !== 'production'){
-//     require('dotenv').load();
-// }
-
-// const secrete_key = process.env.
-
-// console.log()
-
 app.listen(PORT, function () {
     console.log(`Server started on http://localhost:${PORT}`);
 });
