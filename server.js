91532bdbd63c34592c11580bf5969fa80df83b8f
nabin <nkbhaie2017@gmail.com>
2023-01-04 17:57:07 -0700

    change the code in server.js file to work in process.evn mode

--- server.js
+++ server.js
@@ -3,6 +3,12 @@
 const app = require("./app");
 const { PORT } = require("./configs/configurations");
 
-app.listen(PORT, function () {
-    console.log(`Server started on http://localhost:${PORT}`);
-});
+// app.listen(PORT, function () {
+//     console.log(`Server started on http://localhost:${PORT}`);
+// });
+
+
+// app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || PORT, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
