65615e5a92d1f49b977e3beccc9e2bd3c20614cc
nabin <nkbhaie2017@gmail.com>
2023-01-05 11:33:36 -0700

    modify db.js

--- db.js
+++ db.js
@@ -15,7 +15,10 @@ if (process.env.NODE_ENV === "test"){
 }
 
 let db = new Client({
-    connectionString: DB_URI
+    connectionString: DB_URI,
+    ssl: {
+        rejectUnauthorized: false
+      }
 });
 
 // establish a connection to database
