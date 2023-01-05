4b08c6ef82097c31be55894fa179014c964a70f5
nabin <nkbhaie2017@gmail.com>
2023-01-05 10:05:44 -0700

    set up database URL for deployment

--- db.js
+++ db.js
@@ -2,10 +2,16 @@ const { Client } = require('pg');
 
 let DB_URI;
 
-if(process.env.NODE_ENV === "test"){
-    DB_URI = "postgresql:///aecc_test_db";
+// if(process.env.NODE_ENV === "test"){
+//     DB_URI = "postgresql:///aecc_test_db";
+// }else{
+//     DB_URI = "postgresql:///aecc_db"
+// }
+
+if (process.env.NODE_ENV === "test"){
+    DB_URI =  "aecc_test_db"
 }else{
-    DB_URI = "postgresql:///aecc_db"
+    DB_URI =  process.env.DATABASE_URL || "aecc_db";
 }
 
 let db = new Client({
