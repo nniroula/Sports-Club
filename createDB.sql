e987a594e937cec1b11f469bdabd224b665a22af
nabin <nkbhaie2017@gmail.com>
2023-01-05 13:08:58 -0700

    comment databse file

--- createDB.sql
+++ createDB.sql
@@ -8,12 +8,12 @@
 -- \i seed.sql
 
 
-\i dbSchemas.sql;
-\i seed.sql
+-- \i dbSchemas.sql;
+-- \i seed.sql
 
 -- DROP DATABASE aecc_test;
--- DROP DATABASE IF EXISTS aecc_test_db;
--- CREATE DATABASE aecc_test_db;
--- \connect aecc_test_db
+DROP DATABASE IF EXISTS aecc_test_db;
+CREATE DATABASE aecc_test_db;
+\connect aecc_test_db
 
--- \i dbSchemas.sql
+\i dbSchemas.sql
