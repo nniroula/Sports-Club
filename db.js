3565f17d558d76ed284e231f52bd8690872afd33
nabin <nkbhaie2017@gmail.com>
2023-01-05 12:02:32 -0700

    remove ssl false statement

--- db.js
+++ db.js
@@ -16,9 +16,9 @@ if (process.env.NODE_ENV === "test"){
 
 let db = new Client({
     connectionString: DB_URI,
-    ssl: {
-        rejectUnauthorized: false
-      }
+    // ssl: {
+    //     rejectUnauthorized: false
+    //   }
 });
 
 // establish a connection to database
