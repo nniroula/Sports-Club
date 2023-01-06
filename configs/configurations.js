9f8a75d2338211fd17d18aac162cab24e2879a0f
nabin <nkbhaie2017@gmail.com>
2023-01-05 19:12:48 -0700

    remove PORT delcaration

--- configs/configurations.js
+++ configs/configurations.js
@@ -13,11 +13,11 @@ const PORT = process.env.PORT || 3000;
 // const PORT = 3000;
 
 // Use dev database, testing database, or via env var, production database
-function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
-      ? "aecc_test_db"
-      : process.env.DATABASE_URL || "aecc_db";
-}
+// function getDatabaseUri() {
+//   return (process.env.NODE_ENV === "test")
+//       ? "aecc_test_db"
+//       : process.env.DATABASE_URL || "aecc_db";
+// }
 
 
 //
@@ -29,6 +29,6 @@ const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 module.exports = {
     PORT,
     BCRYPT_WORK_FACTOR,
-    SECRET_PASS,
-    getDatabaseUri,
+    SECRET_PASS
+    // getDatabaseUri,
 };
