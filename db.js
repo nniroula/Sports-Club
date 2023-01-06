a842d838bb82a1ca73ef90832319166a35d26e95
nabin <nkbhaie2017@gmail.com>
2023-01-06 10:47:40 -0700

    change aecc_db

--- db.js
+++ db.js
@@ -11,12 +11,13 @@ let DB_URI;
 if (process.env.NODE_ENV === "test"){
     DB_URI =  "aecc_test_db"
 }else{
-    DB_URI = process.env.DATABASE_URL || "aecc_db";
+    // DB_URI = process.env.DATABASE_URL || "aecc_db";
+    DB_URI = process.env.DATABASE_URL;
 }
 
 let db = new Client({
     connectionString: DB_URI,
-    ssl: { rejectUnauthorized: false }
+    // ssl: { rejectUnauthorized: false }
 });
 
 // establish a connection to database
