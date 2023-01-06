edb99d646090503530d55279bcbafbc7a456d1c8
nabin <nkbhaie2017@gmail.com>
2023-01-06 13:09:17 -0700

    add db url

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ let DB_URI;
 // }else{
 //     DB_URI = "postgresql:///aecc_db"
 // }
-
+postgres://qxmrhzhowsjcef:3bbdf03e872156edc986c1d28e2550f5ea32cea55e3affe15dbd358751cdf4d6@ec2-3-229-252-6.compute-1.amazonaws.com:5432/d9mp874l9q8pfa
 if (process.env.NODE_ENV === "test"){
     DB_URI =  "aecc_test_db"
 }else{
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "test"){
 
 let db = new Client({
     connectionString: DB_URI,
-    // ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false }
 });
 
 // establish a connection to database
