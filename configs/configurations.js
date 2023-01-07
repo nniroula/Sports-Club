ed002266636ad6c440bba2fade827714a1b0695a
nabin <nkbhaie2017@gmail.com>
2023-01-07 14:21:20 -0700

    refactor and clean configurations and db js files

--- configs/configurations.js
+++ configs/configurations.js
@@ -3,27 +3,20 @@
 const express = require('express');
 
 require("dotenv").config();
-// const SECRET_KEY = "secrettocricketaecc";
-// const SECRET_KEY = process.env.secretkey || "secrettocricketaecc";
 const SECRET_PASS = process.env.SECRET_KEY || "secrettocricketaecc";
-
-
-// const PORT = +process.env.PORT || 3000;
 const PORT = process.env.PORT || 3000;
-// const PORT = 3000;
 
-// Use dev database, testing database, or via env var, production database
-// function getDatabaseUri() {
-//   return (process.env.NODE_ENV === "test")
-//       ? "aecc_test_db"
-//       : process.env.DATABASE_URL || "aecc_db";
-// }
+// To run locally, create getDatbaseUri and call it in db.js file
+/*
+Use dev database, testing database, or via env var, production database
+function getDatabaseUri() {
+  return (process.env.NODE_ENV === "test")
+      ? "aecc_test_db"
+      : process.env.DATABASE_URL || "aecc_db";
+}
+*/
 
-
-//
-// WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
-
 // console.log("PORT:", PORT.toString());
 
 module.exports = {
--- db.js
+++ db.js
@@ -2,25 +2,36 @@ const { Client } = require('pg');
 
 let DB_URI;
 
-// if(process.env.NODE_ENV === "test"){
-//     DB_URI = "postgresql:///aecc_test_db";
-// }else{
-//     DB_URI = "postgresql:///aecc_db"
-// }
 if (process.env.NODE_ENV === "test"){
     DB_URI =  "aecc_test_db"
 }else{
-    // DB_URI = process.env.DATABASE_URL || "aecc_db";
     DB_URI = process.env.DATABASE_URL;
 }
+
+// To make it run locally, call getDatabaseUri() from configurations.js file
+/*
+if (process.env.NODE_ENV === "production") {
+    client = new Client({
+      connectionString: getDatabaseUri(), // getDatabseUri() is defined in 
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+  } else {
+    client = new Client({
+      connectionString: getDatabaseUri(),
+    });
+  }
+*/
 
 let db = new Client({
     connectionString: DB_URI,
     ssl: { rejectUnauthorized: false }
 });
 
-// establish a connection to database
 db.connect();
 
 module.exports = db;
+
+
 
